b35f05f9dae68e215dd22dca60a161b42fa7668c
Mike Voytovich <mike@currantlabs.com>
2016-08-25 12:02:07 -0700

    add a shell script to test it out

---
+++ examples/uncompress_streaming/test.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+FILENAME=random
+INFILE=$FILENAME.bin
+GZIP_FILE=$INFILE.gz
+OUTFILE=$FILENAME.out
+
+COUNT=$RANDOM*100
+echo "Generating random file, $COUNT bytes"
+
+#generate a random bytestream
+dd if=/dev/urandom of=$INFILE bs=1 count=$COUNT
+
+#compress it
+gzip -k $INFILE
+
+#decompress it with our app
+./tgunzip $GZIP_FILE $OUTFILE
+
+#compare original to decompressed file
+diff $OUTFILE $INFILE
+
+if [ $? != 0 ]; then
+	echo "FAILED!"
+else
+	echo "PASSED!"
+fi
+
+#clean up
+rm $INFILE
+rm $GZIP_FILE
+rm $OUTFILE
