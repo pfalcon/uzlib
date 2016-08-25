5ab0b329f947522186ebd3609ff523f41cc4b3f7
Mike Voytovich <mike@currantlabs.com>
2016-08-25 12:05:29 -0700

    do in a loop

--- examples/uncompress_streaming/test.sh
+++ examples/uncompress_streaming/test.sh
@@ -1,32 +1,42 @@
 #!/bin/sh
 
-FILENAME=random
-INFILE=$FILENAME.bin
-GZIP_FILE=$INFILE.gz
-OUTFILE=$FILENAME.out
+function test {
+	FILENAME=random
+	INFILE=$FILENAME.bin
+	GZIP_FILE=$INFILE.gz
+	OUTFILE=$FILENAME.out
 
-COUNT=$RANDOM*100
-echo "Generating random file, $COUNT bytes"
+	COUNT=$RANDOM*100
+	echo "Generating random file, $COUNT bytes"
 
-#generate a random bytestream
-dd if=/dev/urandom of=$INFILE bs=1 count=$COUNT
+	#generate a random bytestream
+	dd if=/dev/urandom of=$INFILE bs=1 count=$COUNT
 
-#compress it
-gzip -k $INFILE
+	#compress it
+	gzip -k $INFILE
 
-#decompress it with our app
-./tgunzip $GZIP_FILE $OUTFILE
+	#decompress it with our app
+	./tgunzip $GZIP_FILE $OUTFILE
 
-#compare original to decompressed file
-diff $OUTFILE $INFILE
+	#compare original to decompressed file
+	diff $OUTFILE $INFILE
 
-if [ $? != 0 ]; then
-	echo "FAILED!"
-else
-	echo "PASSED!"
-fi
+	if [ $? != 0 ]; then
+		echo "FAILED!"
+		exit 1
+	else
+		echo "PASSED!"
+	fi
 
-#clean up
-rm $INFILE
-rm $GZIP_FILE
-rm $OUTFILE
+	#clean up
+	rm $INFILE
+	rm $GZIP_FILE
+	rm $OUTFILE	
+}
+
+COUNTER=0
+while [  $COUNTER -lt 100 ]; do
+    echo Run $COUNTER
+    test
+    let COUNTER=COUNTER+1 
+done
