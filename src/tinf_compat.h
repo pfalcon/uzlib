9e65bc75e15e9cd125c6b4e63915a50ff9d51fe3
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-07-13 23:02:01 +0300

    uzlib.h: Rename TINF_DATA to struct uzlib_uncomp.
    At the same time, introduce tinf_compat.h for compatibility defines and
    define TINF_DATA as struct uzlib_uncomp to avoid immediate breakage in
    client apps.
    
    Also, reformat struct uzlib_uncomp to consistently have indent of 4.

---
+++ src/tinf_compat.h
@@ -0,0 +1,5 @@
+/* This header contains compatibility defines for the original tinf API
+   and uzlib 2.x and below API. These defines are deprecated and going
+   to be removed in the future, so applications should migrate to new
+   uzlib API. */
+#define TINF_DATA struct uzlib_uncomp
