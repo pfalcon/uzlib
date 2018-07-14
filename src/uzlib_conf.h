4d89f7a8487857a6c40fcc99caa541f1ff6498d5
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-07-15 00:54:47 +0300

    uzlib_conf.h: Introduce a header with configuration settings.
    Starting with debug logging setting.

---
+++ src/uzlib_conf.h
@@ -0,0 +1,15 @@
+/*
+ * uzlib  -  tiny deflate/inflate library (deflate, gzip, zlib)
+ *
+ * Copyright (c) 2014-2018 by Paul Sokolovsky
+ */
+
+#ifndef UZLIB_CONF_H_INCLUDED
+#define UZLIB_CONF_H_INCLUDED
+
+#ifndef UZLIB_CONF_DEBUG_LOG
+/* Debug logging level 0, 1, 2, etc. */
+#define UZLIB_CONF_DEBUG_LOG 0
+#endif
+
+#endif /* UZLIB_CONF_H_INCLUDED */
